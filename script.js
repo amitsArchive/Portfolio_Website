933fac55da9e025d96fdd382951f3acd2937e141
Amit Sharma <amit.sharma.96677@gmail.com>
2025-03-22 16:02:45 +0530

    portfolio website project done

---
+++ script.js
@@ -0,0 +1,24 @@
+// Navbar scroll effect
+window.addEventListener('scroll', function() {
+    const nav = document.querySelector('nav');
+    if (window.scrollY > 50) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
+
+// Tab functionality
+const tabLinks = document.getElementsByClassName("tab-links");
+const tabContents = document.getElementsByClassName("tab-contents");
+
+function openTab(tabName) {
+    for (tabLink of tabLinks) {
+        tabLink.classList.remove("active-link");
+    }
+    for (tabContent of tabContents) {
+        tabContent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabName).classList.add("active-tab");
+}
